83e1933987f4ead7b24374e2e95762716e181d31
AsiriSanda <sadakelum1234@gmail.com>
2022-09-07 21:19:33 +0530

    order detail completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const customer = require('./routes/customer')
 const item = require('./routes/item')
 const order = require('./routes/order')
+const orderDetail = require('./routes/orderDetail')
 const app = express()
 const port = 4000
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use('/customer', customer)
 app.use('/item', item)
 app.use('/order', order)
+app.use('/orderDetail', orderDetail)
 
 app.get('/', (req, res) => {
     console.log("get request coming")
